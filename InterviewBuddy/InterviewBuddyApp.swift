a802a849d5cdd0e0eca2a0df1f8968d9d79bb175
HO Chun Chit <24202495@life.hkbu.edu.hk>
2025-09-04 15:05:58 +0800

    Initial Commit

---
+++ InterviewBuddy/InterviewBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewBuddyApp.swift
+//  InterviewBuddy
+//
+//  Created by 1234 on 4/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct InterviewBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterviewBuddyTests/InterviewBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewBuddyTests.swift
+//  InterviewBuddyTests
+//
+//  Created by 1234 on 4/9/2025.
+//
+
+import Testing
+@testable import InterviewBuddy
+
+struct InterviewBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
